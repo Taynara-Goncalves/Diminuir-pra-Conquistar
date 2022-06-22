1a9af2a8d6a4c875ea3f4e2d2481e92988084247
Taynara-Goncalves <107331651+Taynara-Goncalves@users.noreply.github.com>
2022-06-21 22:04:36 -0300

    Update Insertion Sort.cpp

--- Insertion Sort.cpp
+++ Insertion Sort.cpp
@@ -1,5 +1,10 @@
 // Insertion Sort.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
-//
+
+//Insertion Sort, ou ordenação por inserção, é um algoritmo de ordenação que, dado uma estrutura (array, lista) 
+//constrói uma matriz final com um elemento de cada vez, uma inserção por vez. Assim como algoritmos de ordenação 
+//quadrática, é bastante eficiente para problemas com pequenas entradas, sendo o mais eficiente entre os 
+//algoritmos desta ordem de classificação.
+    
 
 #include <iostream>
 #include <vector>
